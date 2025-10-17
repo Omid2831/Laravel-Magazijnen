1ba44a0c7c390dcfcc28bb6f744a010d667c4f71
Omid <mehrabiomid8282@gmail.com>
2025-10-17 10:26:10 +0200

    feat: create sp_GetAllMagazijnen stored procedure for active product retrieval

--- database/store_precedures/sp_GetAllMagazijnen.sql
+++ database/store_precedures/sp_GetAllMagazijnen.sql
@@ -1,12 +1,21 @@
-DROP PROCEDURE IF EXISTS sp_CreateAllMagazijnen;
-
+USE aryanDBassignment;
+DROP PROCEDURE IF EXISTS sp_GetAllMagazijnen;
 DELIMITER $$
 
-CREATE PROCEDURE sp_CreateAllMagazijnen()
+CREATE PROCEDURE sp_GetAllMagazijnen()
 BEGIN
-     
-    
-    
+    SELECT
+
+         p.Barcode AS barcode
+        ,p.Naam AS naam
+        ,m.VerpakkingsEenheid AS verpakkingseenheid
+        ,m.AantalAanwezig AS aantal_aanwezig
+
+    FROM Magazijn m
+    INNER JOIN Product p ON m.ProductId = p.Id
+    WHERE m.IsActief = 1 AND p.IsActief = 1
+    ORDER BY p.Naam ASC;
+
 END$$
 
 DELIMITER ;
