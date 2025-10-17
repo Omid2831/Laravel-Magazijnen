d6f824270b522fa1ba06f3c037ba267618b0293a
Omid <mehrabiomid8282@gmail.com>
2025-10-17 10:38:14 +0200

    fix: comment out allergenen and leverantie info columns in magazijnen index view

--- resources/views/magazijnen/index.blade.php
+++ resources/views/magazijnen/index.blade.php
@@ -20,8 +20,8 @@
                 <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->naam }}</td>
                 <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->verpakkingseenheid }}</td>
                 <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->aantal_aanwezig }}</td>
-                <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->allergenen_info }}</td>
-                <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->leverantie_info }}</td>
+                {{-- <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->allergenen_info }}</td>
+                <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->leverantie_info }}</td> --}}
             </tr>
            @endforeach
         </tbody>
