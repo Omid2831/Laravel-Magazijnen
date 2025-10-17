4edb2621fd0f5f132e630b40d701f38d056cccc0
Omid <mehrabiomid8282@gmail.com>
2025-10-17 11:12:21 +0200

    style: fix layout issues in magazijnen index view for improved readability

--- resources/views/magazijnen/index.blade.php
+++ resources/views/magazijnen/index.blade.php
@@ -1,32 +1,32 @@
 @extends('magazijnen.layouts.layouts')
 
 @section('content')
-    <h1 class="text-3xl font-bold text-gray-800 mb-6k">Overzicht Magazijn Jamin</h1>
-    <div class="bg-white shadow-lg rounded-lg overflow-hidden border-4 mt-4">
-        <table class="table-fixed border-gray-400 w-full text-center m-4">
-            <thead class="bg-gray-200">
+<h1 class="text-3xl font-bold text-gray-800 mb-6">Overzicht Magazijn Jamin</h1>
+<div class="bg-white shadow-lg rounded-lg overflow-x-auto border-4 mt-4">
+    <table class="table-fixed border-collapse border border-gray-400 w-full text-center">
+        <thead class="bg-gray-200">
+            <tr>
+                <th class="border border-gray-400 px-4 py-2">Barcode</th>
+                <th class="border border-gray-400 px-4 py-2">Naam</th>
+                <th class="border border-gray-400 px-4 py-2">Verpakkingseenheid</th>
+                <th class="border border-gray-400 px-4 py-2">Aantal aanwezig</th>
+                <th class="border border-gray-400 px-4 py-2">Allergenen Info</th>
+                <th class="border border-gray-400 px-4 py-2">Leverantie Info</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($Magazijnen as $Magazijn)
                 <tr>
-                    <th class="border border-gray-400 px-4 py-2">Barcode</th>
-                    <th class="border border-gray-400 px-4 py-2">Naam</th>
-                    <th class="border border-gray-400 px-4 py-2">Verpakkingseenheid</th>
-                    <th class="border border-gray-400 px-4 py-2">Aantal aanwezig</th>
-                    <th class="border border-gray-400 px-4 py-2">Allergenen Info</th>
-                    <th class="border border-gray-400 px-4 py-2">Leverantie Info</th>
+                    <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->barcode }}</td>
+                    <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->naam }}</td>
+                    <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->verpakkingseenheid }}</td>
+                    <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->aantal_aanwezig }}</td>
+                    {{-- Uncomment when data is available --}}
+                    {{-- <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->allergenen_info }}</td>
+                    <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->leverantie_info }}</td> --}}
                 </tr>
-            </thead>
-            <tbody>
-                @foreach ($Magazijnen as $Magazijn)
-                    <tr>
-                        <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->barcode }}</td>
-                        <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->naam }}</td>
-                        <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->verpakkingseenheid }}</td>
-                        <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->aantal_aanwezig }}</td>
-
-                        {{-- <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->allergenen_info }}</td>
-                        <td class="border border-gray-400 px-4 py-2">{{ $Magazijn->leverantie_info }}</td> --}}
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
