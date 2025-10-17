f491420c14351f6426b002e14f90c8ec09cfe708
Omid <mehrabiomid8282@gmail.com>
2025-10-17 10:15:34 +0200

    feat: implement sp_getAllMagazijnen method in MagazijnModel for data retrieval

--- app/Models/MagazijnModel.php
+++ app/Models/MagazijnModel.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class MagazijnModel extends Model
 {
-    //
+    public function sp_getAllMagazijnen()
+    {
+
+        // fetching the data from the database
+        return DB::select('CALL sp_GetAllMagazijnen()');
+    }
 }
