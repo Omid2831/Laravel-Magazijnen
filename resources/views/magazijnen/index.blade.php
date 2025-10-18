7721cd99c4001396d6b361812cc9983a7a173295
Omid2831 <mehrabiomid8282@gmail.com>
2025-10-18 12:58:27 +0200

    style: update layout reference and title in magazijnen index view for consistency

--- resources/views/magazijnen/index.blade.php
+++ resources/views/magazijnen/index.blade.php
@@ -1,7 +1,7 @@
-@extends('magazijnen.layouts.layouts')
+@extends('magazijnen.layouts.magazijnen')
 
 @section('content')
-    <h1 class="text-3xl font-bold text-gray-800 mb-6">Overzicht Magazijn Jamin</h1>
+    <h1 class="text-3xl font-bold text-gray-800 mb-6">{{ $title }}</h1>
     <div class="bg-white shadow-lg rounded-lg overflow-x-auto border-4 mt-4">
         <table class="table-fixed border-collapse border border-gray-400 w-full text-center">
             <thead class="bg-gray-200">
@@ -27,12 +27,12 @@
                                 View Product
                             </a>
                         </td>
-                        <td class="border border-gray-400 px-4 py-2">
-                            <a href=""
-                                class="text-gray-500 hover:text-blue-600 hover:drop-shadow-lg hover:shadow-pink-500/50 transition-all duration-300 hover:font-semibold hover:scale-105">
-                                View Product
+                        {{-- <td class="border border-gray-400 px-4 py-2">
+                            <a href="{{ route('leverancier.show', $Magazijn->Id ?? $Magazijn->Id) }}"
+                                class="text-gray-500 hover:text-pink-600 hover:drop-shadow-lg hover:shadow-pink-500/50 transition-all duration-300 hover:font-semibold hover:scale-105">
+                                Leverantie Info
                             </a>
-                        </td>
+                        </td> --}}
                     </tr>
                 @endforeach
             </tbody>
