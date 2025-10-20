1c5afb025d197f6ad19b2eb704e94d6cdd517259
Omid2831 <mehrabiomid8282@gmail.com>
2025-10-20 09:44:28 +0200

    feat: add formatted date column to sp_GetAllAllergenen procedure

--- database/store_precedures/sp_GetAllAllergenen.sql
+++ database/store_precedures/sp_GetAllAllergenen.sql
@@ -1,3 +1,5 @@
+USE aryanDBassignment;
+
 DROP PROCEDURE IF EXISTS sp_GetAllAllergenen;
 
 DELIMITER $$
@@ -8,6 +10,7 @@ BEGIN
     SELECT ALGE.Id
           ,ALGE.Naam
           ,ALGE.Omschrijving
+          ,FORMAT(ALGE.DatumGewijzigd, '%d-%m-%Y') AS DatumGewijzigd
     FROM Allergeen as ALGE;
 
 END$$
