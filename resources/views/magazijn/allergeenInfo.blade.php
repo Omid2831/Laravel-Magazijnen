3a288b5e5f0c04d4bb8c6bbfbfa5a200bf3df5be
Omid2831 <mehrabiomid8282@gmail.com>
2025-10-20 11:49:08 +0200

    feat: create allergeen layout and update allergeenInfo view to display product details

--- resources/views/magazijn/allergeenInfo.blade.php
+++ resources/views/magazijn/allergeenInfo.blade.php
@@ -1,8 +1,13 @@
-@foreach($allergeen as $item)
-    <tr>
-        <td>{{ $item->Id }}</td>
-        <td>{{ $item->Naam }}</td>
-        <td>{{ $item->Omschrijving }}</td>
-        <td>{{ $item->DatumGewijzigd }}</td>
-    </tr>
-@endforeach
+@extends('magazijn.layouts.allergeen')
+
+@section('allergeenInfo')
+    <h1 class='text-3xl font-bold text-gray-800 mb-6'>{{ $title }}</h1>
+
+    <div class="mb-4 border-4 p-4 m-4 rounded w-100 bg-yellow-50">
+        @foreach ($products as $product)
+            <p><strong>Allergeen Naam:</strong> {{ $product->Naam ?? 'N/A' }}</p>
+            <p><strong>Barcode:</strong> {{ $product->Barcode ?? 'N/A' }}</p>
+        @endforeach
+    </div>
+
+@endsection
