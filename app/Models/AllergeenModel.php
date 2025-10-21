968dcc831297df76aed09b45c49b7ef55c8f20d7
Omid2831 <mehrabiomid8282@gmail.com>
2025-10-21 15:39:03 +0200

    feat: implement method to retrieve all allergen data from stored procedure

--- app/Models/AllergeenModel.php
+++ app/Models/AllergeenModel.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class AllergeenModel extends Model
 {
-    //
+    public function getAllAllergenenData()
+    {
+        return DB::select('CALL sp_GetAllergenen()');
+    }
 }
