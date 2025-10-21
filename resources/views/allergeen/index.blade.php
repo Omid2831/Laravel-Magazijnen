f96033144c9bec239c1c72721ad212097a0c6248
Omid2831 <mehrabiomid8282@gmail.com>
2025-10-21 16:00:22 +0200

    feat: add link to create new allergen in index view

--- resources/views/allergeen/index.blade.php
+++ resources/views/allergeen/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
     <h2 class='text-center text-3xl font-bold text-shadow'>{{ $Metadata['title'] }}</h2>
+    <a href="{{ route('allergeen.create') }}"
+        class="inline-block bg-gray-100 hover:bg-gray-200 text-gray-800 font-medium px-5 py-2 rounded-lg border border-gray-300 transition-colors duration-200">
+        Nieuw Allergeen
+    </a>
+
     <div class="bg-white shadow-lg rounded-lg overflow-x-auto border-4 mt-4">
         <table class="table-fixed border-collapse border border-gray-400 w-full text-center">
             <thead class="bg-gray-200">
