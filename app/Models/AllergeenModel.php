32528517ec9d0af4d312f4da6ae8457e9fa703fc
Omid2831 <mehrabiomid8282@gmail.com>
2025-10-21 21:28:56 +0200

    feat: add createAllergeen method to AllergeenModel for allergen creation

--- app/Models/AllergeenModel.php
+++ app/Models/AllergeenModel.php
@@ -11,4 +11,12 @@ class AllergeenModel extends Model
     {
         return DB::select('CALL sp_GetAllergenen()');
     }
+
+    /*
+     * Create a new allergen
+     */
+    public static function createAllergeen($naam, $omschrijving)
+    {
+        DB::selectOne('CALL sp_CreateAllergenen(?, ?)', [$naam, $omschrijving]);
+    }
 }
