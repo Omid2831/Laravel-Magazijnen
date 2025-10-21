fa24b6bd9b2567bf39c8c1502cfcbacc2376524c
Omid2831 <mehrabiomid8282@gmail.com>
2025-10-21 15:39:27 +0200

    feat: create allergen index view with layout and content structure

--- resources/views/allergeen/index.blade.php
+++ resources/views/allergeen/index.blade.php
@@ -1,1 +1,23 @@
-<h2>Allergenen page</h2>
+@extends('allergeen.layouts.index')
+
+@section('content')
+    <h2 class='text-center text-3xl font-bold text-shadow'>{{ $Metadata['title'] }}</h2>
+    <div class="bg-white shadow-lg rounded-lg overflow-x-auto border-4 mt-4">
+        <table class="table-fixed border-collapse border border-gray-400 w-full text-center">
+            <thead class="bg-gray-200">
+                <tr>
+                    <th class="border border-gray-400 px-4 py-2">Naam</th>
+                    <th class="border border-gray-400 px-4 py-2">Omschrijving</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($allergeen as $item)
+                    <tr>
+                        <td class="border border-gray-400 px-4 py-2">{{ $item->Naam }}</td>
+                        <td class="border border-gray-400 px-4 py-2">{{ $item->Omschrijving }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
