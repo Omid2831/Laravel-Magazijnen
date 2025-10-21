21132303634f2c704107527a33d1fc358b0ed2b2
Omid2831 <mehrabiomid8282@gmail.com>
2025-10-21 21:28:48 +0200

    fix: improve error handling and metadata setup in allergen index and create methods

--- app/Http/Controllers/AllergeenController.php
+++ app/Http/Controllers/AllergeenController.php
@@ -19,35 +19,38 @@ class AllergeenController extends Controller
     public function index()
     {
 
-       try{
-         // get the feched data from the store procedure
-        $allergeen = $this->allergeenModel->getAllAllergenenData();
+        try {
+            // get the feched data from the store procedure
+            $allergeen = $this->allergeenModel->getAllAllergenenData();
 
-        
-        // set the metadata for the view here and pass the fetcched data to the view
-        $Metadata = [
-            'title' => 'Allergenen Overzicht',
-        ];
 
-        // return the view with the data and metadata
-        return view('allergeen.index', compact('Metadata', 'allergeen'));
+            // set the metadata for the view here and pass the fetcched data to the view
+            $Metadata = [
+                'title' => 'Allergenen Overzicht',
+            ];
 
-       }catch(\Exception $e){
-        // in case of error, return back with the error message
-        Log::error('Error fetching allergenen data: ' . $e->getMessage());
+            // return the view with the data and metadata
+            return view('allergeen.index', compact('Metadata', 'allergeen'));
+        } catch (\Exception $e) {
+            // in case of error, return back with the error message
+            Log::error('Error fetching allergenen data: ' . $e->getMessage());
 
-        // return back with an error message
-        return back()->withErrors('Er is een fout opgetreden bij het ophalen van de allergenen gegevens.');
-
-       }
+            // return back with an error message
+            return back()->withErrors('Er is een fout opgetreden bij het ophalen van de allergenen gegevens.');
+        }
     }
 
     /**
      * Show the form for creating a new resource.
      */
     public function create()
-    {
-        return view('allergeen.create');
+    {  
+        $allergeen = $this->allergeenModel->getAllAllergenenData();
+        return view('allergeen.create', [
+            'title' => 'Nieuwe Record aanmaken',
+            'description' => 'Hier kunt u een nieuwe allergeeen aanmaken.',
+            'allergeen' => $allergeen
+        ]);
     }
 
     /**
@@ -55,7 +58,27 @@ class AllergeenController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $validatedData = $request->validate([
+                'naam' => 'required|string|max:50',
+                'omschrijving' => 'required|string',
+            ]);
+
+            $allergeen = AllergeenModel::createAllergeen(
+                $validatedData['naam'],
+                $validatedData['omschrijving']
+            );
+
+            if (!$allergeen) {
+                throw new \Exception('Stored procedure returned no result.');
+            }
+
+            return redirect()->route('allergeen.index')
+                ->with('success', 'Allergeen succesvol aangemaakt.');
+        } catch (\Exception $e) {
+            Log::error('Error creating allergen: ' . $e->getMessage());
+            return back()->withErrors('Er is een fout opgetreden bij het aanmaken van het allergeen.')->withInput();
+        }
     }
 
     /**
