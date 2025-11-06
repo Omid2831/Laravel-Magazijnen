627829f158ec5c1288741fd943e3fd681f751423
Omid2831 <mehrabiomid8282@gmail.com>
2025-11-06 12:36:33 +0100

    feat: add LeverancierModel with initial table definition and method stub

---
+++ app/Models/LeverancierModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LeverancierModel extends Model
+{
+    protected $table =['Leverancier'];
+
+    public function getAllLeverancierData()
+    {
+        
+    }
+}
