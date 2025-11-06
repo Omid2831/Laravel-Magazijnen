70383639ae4fac5cf9b1b857c716f5c3007df7ba
Omid2831 <mehrabiomid8282@gmail.com>
2025-11-06 10:49:22 +0100

    fix: correct layout and route references for leverancier index page

--- resources/views/leverancier/index.blade.php
+++ resources/views/leverancier/index.blade.php
@@ -1,9 +1,9 @@
-@extends('allergeen.layouts.index')
+@extends('leverancier.layouts.index')
 
 @section('content')
     <x-alerts />
     <h2 class='text-center text-3xl font-bold text-shadow'>{{ $Metadata['title'] }}</h2>
-    <a href="{{ route('allergeen.create') }}"
+    <a href="{{ route('leverancier.create') }}"
         class="inline-block bg-gray-100 hover:bg-gray-200 text-gray-800 font-medium px-5 py-2 rounded-lg border border-gray-300 transition-colors duration-200">
         Overzicht Leverancier   
     </a>
